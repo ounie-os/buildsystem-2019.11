0ca5ef8a5c9c7fe2ddc10659c55f5e941c91fe27
ounie-os <ounie@qq.com>
2020-01-29 14:40:36 +0800

    针对beaglebone black的配置文件

---
+++ board/beaglebone/utils.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -u
+set -e
+
+BOARD_DIR="$(dirname $0)"
+WIFI_FIRMWARE="${BOARD_DIR}/rtlwifi"
+WIFI_SCRIPT="${BOARD_DIR}/init_scripts"
+
+mkdir -p ${TARGET_DIR}/lib/firmware
+cp -ar ${WIFI_FIRMWARE} ${TARGET_DIR}/lib/firmware
+cp -ar ${WIFI_SCRIPT}/* ${TARGET_DIR}/etc/init.d
+
