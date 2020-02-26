39218f335dcdb41e94a4a9f7f12f588799e27238
ounie-os <ounie@qq.com>
2020-02-26 10:14:08 +0800

    修正postbuild-bt.sh脚本

--- board/beaglebone/postbuild-bt.sh
+++ board/beaglebone/postbuild-bt.sh
@@ -6,9 +6,11 @@ set -e
 BOARD_DIR="$(dirname $0)"
 WIFI_FIRMWARE="${BOARD_DIR}/rtlwifi"
 INIT_SCRIPT="${BOARD_DIR}/init_scripts_bt"
+BOA_ARIA2="${BOARD_DIR}/boa-AriaNg"
 
 mkdir -p ${TARGET_DIR}/lib/firmware
 cp -ar ${WIFI_FIRMWARE} ${TARGET_DIR}/lib/firmware
 cp -ar ${INIT_SCRIPT}/* ${TARGET_DIR}/etc/init.d
+cp -ar ${BOA_ARIA2}/ ${TARGET_DIR}/etc
 sed -i "5s/'# '/'\\\u@\\\h:\\\w\\\\$ '/g" ${TARGET_DIR}/etc/profile
 sed -i "7s/'$ '/'\\\u@\\\h:\\\w\\\\$ '/g" ${TARGET_DIR}/etc/profile
