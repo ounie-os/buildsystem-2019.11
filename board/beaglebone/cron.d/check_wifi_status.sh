b65c809b7c9a03fcbdf8611fd1f149ac10d99fa6
ounie-os <ounie@qq.com>
2020-03-26 16:03:46 +0800

    更新wifi断开重新的脚本

--- board/beaglebone/cron.d/check_wifi_status.sh
+++ board/beaglebone/cron.d/check_wifi_status.sh
@@ -1,6 +1,29 @@
 #! /bin/sh
 
+INTERFACE=wlan0
+
 /etc/cron.d/ping.sh
 # ifconfig wlan0 | grep RUNNING
-[ $? = 0 ] || $1 $2
+# [ $? = 0 ] || $1 $2
+if [ $? != 0 ]; then
+  kill -15 $(pidof wpa_supplicant)
+  while true
+  do
+    if [ -e /var/run/wpa_supplicant ]; then
+      sleep 1
+    else
+      break
+    fi
+  done
+  wpa_supplicant -s -B -Dnl80211 -i${INTERFACE} -c/etc/wpa_supplicant.conf
+  while true
+  do
+    ifconfig ${INTERFACE} | grep RUNNING
+    if [ $? = 0 ]; then
+      break
+    fi
+    sleep 1
+  done
+  kill -10 $(pidof udhcpc)
+fi
 
