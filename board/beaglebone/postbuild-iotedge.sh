868f6974fb1f601bdd99f2ad7c9cd72db0a5246b
ounie-os <ounie@qq.com>
2020-04-17 20:32:05 +0800

    iot开发基本配置

---
+++ board/beaglebone/postbuild-iotedge.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -u
+set -e
+
+BOARD_DIR="$(dirname $0)"
+WIFI_FIRMWARE="${BOARD_DIR}/rtlwifi"
+INIT_SCRIPT="${BOARD_DIR}/init_systemd"
+
+mkdir -p ${TARGET_DIR}/lib/firmware
+cp -ar ${WIFI_FIRMWARE} ${TARGET_DIR}/lib/firmware
+cp -ar ${BOARD_DIR}/daemon.json ${TARGET_DIR}/etc
+cp -ar ${INIT_SCRIPT}/usbgadget ${TARGET_DIR}/usr/bin
+cp -ar ${INIT_SCRIPT}/usbgadget.service ${TARGET_DIR}/usr/lib/systemd/system
+ln -sf /usr/lib/systemd/system/usbgadget.service \
+	${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/usbgadget.service
+cp -ar ${INIT_SCRIPT}/mountdisk.service ${TARGET_DIR}/usr/lib/systemd/system
+ln -sf /usr/lib/systemd/system/mountdisk.service \
+	${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/mountdisk.service
+sed -i "5s/'# '/'\\\u@\\\h:\\\w\\\\$ '/g" ${TARGET_DIR}/etc/profile
+sed -i "7s/'$ '/'\\\u@\\\h:\\\w\\\\$ '/g" ${TARGET_DIR}/etc/profile
