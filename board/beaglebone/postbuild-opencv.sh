b356885ec45882a54b332e7644619feb644b9587
ounie-os <ounie@qq.com>
2020-05-06 16:00:00 +0800

    运行opencv的配置修改

--- board/beaglebone/postbuild-opencv.sh
+++ board/beaglebone/postbuild-opencv.sh
@@ -5,11 +5,14 @@ set -e
 
 BOARD_DIR="$(dirname $0)"
 WIFI_FIRMWARE="${BOARD_DIR}/rtlwifi"
-INIT_SCRIPT="${BOARD_DIR}/init_scripts_opencv"
+INIT_SCRIPT="${BOARD_DIR}/init_scripts"
+INIT_SCRIPT_OPENCV="${BOARD_DIR}/init_scripts_opencv"
 
 mkdir -p ${TARGET_DIR}/lib/firmware
 cp -ar ${WIFI_FIRMWARE} ${TARGET_DIR}/lib/firmware
 cp -ar ${INIT_SCRIPT}/* ${TARGET_DIR}/etc/init.d
+cp -ar ${INIT_SCRIPT_OPENCV}/* ${TARGET_DIR}/etc/init.d
+cp -ar ${BOARD_DIR}/dnsmasq.conf ${TARGET_DIR}/etc/
 sed -i "5s/'# '/'\\\u@\\\h:\\\w\\\\$ '/g" ${TARGET_DIR}/etc/profile
 sed -i "7s/'$ '/'\\\u@\\\h:\\\w\\\\$ '/g" ${TARGET_DIR}/etc/profile
 
