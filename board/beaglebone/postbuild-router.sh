b19497388c9636c7658d96c6e6d62a4d0a9f923d
ounie-os <ounie@qq.com>
2020-05-14 06:45:17 +0800

    路由器中增加aria2下载器; 网卡接口配置修改; 增加dhcp服务器脚本

--- board/beaglebone/postbuild-router.sh
+++ board/beaglebone/postbuild-router.sh
@@ -7,6 +7,7 @@ BOARD_DIR="$(dirname $0)"
 WIFI_FIRMWARE="${BOARD_DIR}/rtlwifi"
 INIT_SCRIPT="${BOARD_DIR}/init_scripts"
 INIT_SCRIPT_ROUTER="${BOARD_DIR}/init_scripts_router"
+BOA_ARIA2="${BOARD_DIR}/boa-AriaNg"
 
 mkdir -p ${TARGET_DIR}/lib/firmware
 cp -ar ${WIFI_FIRMWARE} ${TARGET_DIR}/lib/firmware
@@ -15,6 +16,8 @@ cp -ar ${INIT_SCRIPT_ROUTER}/* ${TARGET_DIR}/etc/init.d
 cp -ar ${BOARD_DIR}/dnsmasq.conf ${TARGET_DIR}/etc/
 cp -ar ${BOARD_DIR}/hostapd.conf ${TARGET_DIR}/etc/
 cp -ar ${BOARD_DIR}/hostapd-5g.conf ${TARGET_DIR}/etc/
+cp -ar ${BOARD_DIR}/udhcpd.conf ${TARGET_DIR}/etc/
+cp -ar ${BOA_ARIA2}/ ${TARGET_DIR}/etc
 sed -i "5s/'# '/'\\\u@\\\h:\\\w\\\\$ '/g" ${TARGET_DIR}/etc/profile
 sed -i "7s/'$ '/'\\\u@\\\h:\\\w\\\\$ '/g" ${TARGET_DIR}/etc/profile
 
