3d115be0b25115de5ad385df38afb21c67711d53
ounie-os <ounie@qq.com>
2020-10-07 15:16:25 +0800

    路由器配置：系统启动改为systemd。dns转发使用bind。

--- board/beaglebone/postbuild-router.sh
+++ board/beaglebone/postbuild-router.sh
@@ -5,22 +5,31 @@ set -e
 
 BOARD_DIR="$(dirname $0)"
 WIFI_FIRMWARE="${BOARD_DIR}/rtlwifi"
-INIT_SCRIPT="${BOARD_DIR}/init_scripts"
 INIT_SCRIPT_ROUTER="${BOARD_DIR}/init_scripts_router"
 BOA_ARIA2="${BOARD_DIR}/boa-AriaNg"
-CROND="${BOARD_DIR}/cron.d"
 
 mkdir -p ${TARGET_DIR}/lib/firmware
 cp -ar ${WIFI_FIRMWARE} ${TARGET_DIR}/lib/firmware
-cp -ar ${INIT_SCRIPT}/* ${TARGET_DIR}/etc/init.d
-cp -ar ${INIT_SCRIPT_ROUTER}/* ${TARGET_DIR}/etc/init.d
-cp -ar ${BOARD_DIR}/dnsmasq.conf ${TARGET_DIR}/etc/
 cp -ar ${BOARD_DIR}/hostapd.conf ${TARGET_DIR}/etc/
 cp -ar ${BOARD_DIR}/hostapd-5g.conf ${TARGET_DIR}/etc/
-cp -ar ${BOARD_DIR}/udhcpd.conf ${TARGET_DIR}/etc/
 cp -ar ${BOARD_DIR}/sysctl.conf ${TARGET_DIR}/etc/
 cp -ar ${BOA_ARIA2}/ ${TARGET_DIR}/etc
-cp -ar ${CROND}/cron_change_governor ${TARGET_DIR}/etc
 sed -i "5s/'# '/'\\\u@\\\h:\\\w\\\\$ '/g" ${TARGET_DIR}/etc/profile
 sed -i "7s/'$ '/'\\\u@\\\h:\\\w\\\\$ '/g" ${TARGET_DIR}/etc/profile
 
+mkdir -p ${TARGET_DIR}/etc/default
+cp -ar ${INIT_SCRIPT_ROUTER}/dhcpd ${TARGET_DIR}/etc/default/
+cp -ar ${INIT_SCRIPT_ROUTER}/dhcpd.conf ${TARGET_DIR}/etc/dhcp/
+cp -ar ${INIT_SCRIPT_ROUTER}/bin/natScript ${TARGET_DIR}/usr/sbin/natScript
+cp -ar ${INIT_SCRIPT_ROUTER}/journald.conf ${TARGET_DIR}/etc/systemd/
+cp -ar ${INIT_SCRIPT_ROUTER}/resolved.conf ${TARGET_DIR}/etc/systemd/
+cp -ar ${INIT_SCRIPT_ROUTER}/named.conf ${TARGET_DIR}/etc/
+
+cp -ar ${INIT_SCRIPT_ROUTER}/service/* ${TARGET_DIR}/usr/lib/systemd/system/
+
+SERVICES="eth0-static mountdisk nat wifi"
+for x in ${SERVICES}
+do
+ln -sf /usr/lib/systemd/system/${x}.service \
+	${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/${x}.service
+done
