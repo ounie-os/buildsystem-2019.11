23faac5541ebe1510261eb5f6f36d5585061809d
ounie-os <ounie@qq.com>
2021-05-25 09:35:04 +0800

    修改打包脚本

--- board/beaglebone/postbuild-iotedge.sh
+++ board/beaglebone/postbuild-iotedge.sh
@@ -5,7 +5,8 @@ set -e
 
 BOARD_DIR="$(dirname $0)"
 WIFI_FIRMWARE="${BOARD_DIR}/rtlwifi"
-INIT_SCRIPT="${BOARD_DIR}/init_systemd"
+INIT_SCRIPT_SERVICE="${BOARD_DIR}/init_systemd/service"
+INIT_SCRIPT_BIN="${BOARD_DIR}/init_systemd/bin"
 
 mkdir -p ${TARGET_DIR}/lib/firmware
 cp -ar ${WIFI_FIRMWARE} ${TARGET_DIR}/lib/firmware/
@@ -13,14 +14,15 @@ cp -ar ${WIFI_FIRMWARE} ${TARGET_DIR}/lib/firmware/
 mkdir -p ${TARGET_DIR}/etc/docker
 cp -ar ${BOARD_DIR}/daemon.json ${TARGET_DIR}/etc/docker/
 
-cp -ar ${INIT_SCRIPT}/usbgadget ${TARGET_DIR}/usr/bin/
 cp -ar ${BOARD_DIR}/journald.conf ${TARGET_DIR}/etc/systemd/
 
-cp -ar ${INIT_SCRIPT}/usbgadget.service ${TARGET_DIR}/usr/lib/systemd/system/
-ln -sf /usr/lib/systemd/system/usbgadget.service \
-	${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/usbgadget.service
-cp -ar ${INIT_SCRIPT}/mountdisk.service ${TARGET_DIR}/usr/lib/systemd/system/
-ln -sf /usr/lib/systemd/system/mountdisk.service \
-	${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/mountdisk.service
+cp -ar ${INIT_SCRIPT_SERVICE}/* ${TARGET_DIR}/usr/lib/systemd/system/
+cp -ar ${INIT_SCRIPT_BIN}/* ${TARGET_DIR}/usr/bin/
+SERVICES="ze08-iot usbgadget mountdisk mqtt-jetlinks gpio dbus-session coap-server shtc1 wpa_supplicant-nl80211"
+for x in ${SERVICES}
+do
+ln -sf /usr/lib/systemd/system/${x}.service \
+	${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/${x}.service
+done
 sed -i "5s/'# '/'\\\u@\\\h:\\\w\\\\$ '/g" ${TARGET_DIR}/etc/profile
 sed -i "7s/'$ '/'\\\u@\\\h:\\\w\\\\$ '/g" ${TARGET_DIR}/etc/profile
